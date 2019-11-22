d7c75f88fce74276fdd79a80b0a6e7ae3e95ae79
asins <asinsimple@alibaba-inc.com>
2019-11-22 14:40:19 +0800

    清理无用内容

--- src/page/tsx/main.tsx
+++ src/page/tsx/main.tsx
@@ -26,10 +26,6 @@ export default class World extends Vue {
   }
 
   protected render (h: CreateElement): VNode {
-    // const childSlots = {
-    //   default: <span>一般slot</span>,
-    // };
-
     const childScopedSlots = {
       default: <span>一般slot</span>,
       slotName: (res: string) => <span>这里是slot=slotName输出的内容：{res}</span>
@@ -38,11 +34,10 @@ export default class World extends Vue {
       <div>
         <h4>tsx方式的事件交互：</h4>
         <p><input type="text" placeholder="请输入" domPropsValue={this.value} onInput={ this.handleInput } /></p>
+        <p>CSS Modules的使用测试（按钮变为黄色），使用时文件名必须是.module.less结尾： <button class={styles_module.btn} onClick={() => this.value = '' }>Clear</button></p>
         <p>data数据变更到DOM：输入内容：{this.value}</p>
         <p>computed能力测试： {this.valueLength}</p>
         <p>Watch能力测试： {this.msg}</p>
-        <p>CSS Modules的使用测试（按钮变为黄色），使用时文件名必须是.module.less结尾： <button class={styles_module.btn} onClick={() => this.value = '' }>Clear</button></p>
-        {/* <p>子组件slots引用测试：<Child msg="这里是向子组件传入的参数值" slots={childSlots}></Child></p> */}
         <p>
           子组件scoped-slots引用测试：
           <Child msg="这里是向子组件传入的参数值" scopedSlots={childScopedSlots}>
@@ -51,6 +46,7 @@ export default class World extends Vue {
           </Child>
         </p>
       </div>
+      <hr />
       <div>
         <h4><del>v-modle方式的事件交互：能支持vue的.trim等方式</del></h4>
         <p>用这这种写法需在.babelrc中追加<code>presets:["@vue/babel-preset-jsx"]</code>，但写法与vue以及jsx的都一样，而且在TS中不允许冒号出现在属性名称上无法使用v-on功能，所以不推荐使用这个包，既然选择了tsx就忘记vue的修饰符吧！</p>
